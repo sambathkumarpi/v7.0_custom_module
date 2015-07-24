ea4217e14921176b7f61b3e33898032d16c21bbd
Sambath, Sodexis <sambathkumarpi@gmail.com>
2015-07-24 10:53:55 +0530

    Web widget radio

---
+++ web_widget_radio/__openerp__.py
@@ -0,0 +1,10 @@
+{
+	'name':"radio",
+	'depends':["web"],
+	'js': ['static/src/js/widget_radio.js'],
+	'qweb': ['static/src/xml/widget_radio.xml'],
+	'installable': True,
+    'active': True,
+    'auto_install': True,
+}
+	
